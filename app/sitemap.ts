09b1cd7a2450b87037a3074b9306805aeb442dfa
Capumaf <cesarpumayallaf@gmail.com>
2026-01-28 01:33:06 +0100

    Fix sitemap route

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,5 +6,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://ninan-studio.vercel.app",
       lastModified: new Date(),
     },
+    {
+      url: "https://ninan-studio.vercel.app/#about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ninan-studio.vercel.app/#work",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ninan-studio.vercel.app/#services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ninan-studio.vercel.app/#process",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ninan-studio.vercel.app/#contact",
+      lastModified: new Date(),
+    },
   ];
 }
